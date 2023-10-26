34211859369cfb06e44fa1dea6417fd832e6085b
nsetty <nsett7388@gmail.com>
2023-10-26 15:11:43 -0400

    added code comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # calender-application
+
+## Description
+This daily planner application helps the user plan their day from 9AM-5PM. It does this by allowing the user to type in data into different hour slots to plan their day. The application also saves the data to local storage when the save button is pressed and displays it so the data is not lost of the page is closed. The application also displays the current date and time, which updates every minute.
--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 $(function () {
-  const currentHour = new Date().getHours(); 
+  //get the current hour and logs it to local storage to make sure it is working correctly
+  var currentHour = new Date().getHours();
   console.log(currentHour);
-  
-  const saveBtnEl = $(".saveBtn")
 
-  saveBtnEl.on("click", function(){ 
-    let time = $(this).parent().attr("id")
-    let task = $(this).siblings(".description").val()
-    localStorage.setItem(time, task); 
-  })
- 
+  //gets the save buttton class and 
+  var saveBtnEl = $(".saveBtn");
 
-function updateCurrentTime() { 
-  let currentTime = dayjs();
-  let formattedTime = currentTime.format("h:mm A");
-  let formattedDate = currentTime.format("dddd, MMMM D, YYYY");
-  document.getElementById('currentTime').textContent = "Current Time: " + formattedTime;
-  document.getElementById('currentDay').textContent = "Date: " + formattedDate;
-}
-
-updateCurrentTime();
-
-setInterval(updateCurrentTime, 60000); 
-  
-
-$(".time-block").each(function () { 
-  const blockHour = parseInt($(this).attr("id").split("-")[1]); 
-  if (blockHour < currentHour) {
-    $(this).addClass('past');
-   } else if (blockHour === currentHour) {
-    $(this).removeClass('past');
-    $(this).addClass('present');
-   } else {
-    $(this).removeClass('past');
-    $(this).removeClass('present');
-    $(this).addClass('future');
-   }
+  saveBtnEl.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var task = $(this).siblings(".description").val();
+    localStorage.setItem(time, task);
   });
 
+  function updateCurrentTime() {
+    var currentTime = dayjs();
+    var formattedTime = currentTime.format("h:mm A");
+    var formattedDate = currentTime.format("dddd, MMMM D, YYYY");
+    document.getElementById("currentTime").textContent =
+      "Current Time: " + formattedTime;
+    document.getElementById("currentDay").textContent =
+      "Date: " + formattedDate;
+  }
 
-  for (let i = 9; i < 17; i++){ 
-  $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i))}
-  
+  updateCurrentTime();
+
+  setInterval(updateCurrentTime, 60000);
+
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+
+  for (let i = 9; i < 17; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+  }
 });
